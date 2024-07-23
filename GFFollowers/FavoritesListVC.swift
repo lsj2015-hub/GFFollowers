9e82ae46a54d440909d681567daaa35c563061aa
lsj2015-hub <lsj2015@hotmail.com>
2024-07-23 16:23:30 +0900

    App Navigation Setup - UITabBarController & UINavigationController

---
+++ GFFollowers/FavoritesListVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListVC.swift
+//  GFFollowers
+//
+//  Created by Sean Lee on 7/23/24.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBlue
+  }
+}
---
+++ GFFollowers/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  GFFollowers
+//
+//  Created by Sean Lee on 7/23/24.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemPink
+    
+  }
+}
