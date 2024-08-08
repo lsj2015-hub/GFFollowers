522c03f5084e1c7d2f3d822e2390c44393fb1463
lsj2015-hub <lsj2015@hotmail.com>
2024-08-08 15:58:28 +0800

    passing data & keyboards

---
+++ GFFollowers/FollowerListVC.swift
@@ -0,0 +1,20 @@
+//
+//  FollowerListVC.swift
+//  GFFollowers
+//
+//  Created by Sean Lee on 8/7/24.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+  
+  var username: String!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBackground
+    navigationController?.isNavigationBarHidden = false
+    navigationController?.navigationBar.prefersLargeTitles = true
+  }
+}
