a9147c3815890a38022c8f8c81acece03b780dbf
lsj2015-hub <lsj2015@hotmail.com>
2024-08-16 16:14:37 +0800

    Child View Controller - UserInfoHeaderVC

--- GFFollowers/Screens/UserInfoVC.swift
+++ GFFollowers/Screens/UserInfoVC.swift
@@ -9,24 +9,80 @@ import UIKit
 
 class UserInfoVC: UIViewController {
   
+  let headerView  = UIView()
+  let itemViewOne = UIView()
+  let itemViewTwo = UIView()
+  
   var username: String!
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    configureViewController()
+    layoutUI()
+    getUserInfo()
+  }
+  
+  func configureViewController() {
     view.backgroundColor = .systemBackground
     let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
     navigationItem.rightBarButtonItem = doneButton
-    
+  }
+  
+  func getUserInfo() {
     NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
       guard let self = self else { return }
       
       switch result {
       case .success(let user):
-        print(user)
+        DispatchQueue.main.async {
+//          print(user)
+          self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView)
+        }
       case .failure(let error):
         self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
       }
     }
+  }
+  
+  func layoutUI() {
+    view.addSubview(headerView)
+    view.addSubview(itemViewOne)
+    view.addSubview(itemViewTwo)
+    
+    //    headerView.backgroundColor = .systemBlue
+    itemViewOne.backgroundColor = .systemPink
+    itemViewTwo.backgroundColor = .systemBlue
+    
+    headerView.translatesAutoresizingMaskIntoConstraints = false
+    itemViewOne.translatesAutoresizingMaskIntoConstraints = false
+    itemViewTwo.translatesAutoresizingMaskIntoConstraints = false
+    
+    let padding: CGFloat    = 20
+    let itemHeight: CGFloat = 140
+    
+    NSLayoutConstraint.activate([
+      headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      headerView.heightAnchor.constraint(equalToConstant: 180),
+      
+      itemViewOne.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: padding),
+      itemViewOne.leadingAnchor.constraint(equalTo:view.leadingAnchor, constant: padding),
+      itemViewOne.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+      itemViewOne.heightAnchor.constraint(equalToConstant: itemHeight),
+      
+      itemViewTwo.topAnchor.constraint(equalTo: itemViewOne.bottomAnchor, constant: padding),
+      itemViewTwo.leadingAnchor.constraint(equalTo:view.leadingAnchor, constant: padding),
+      itemViewTwo.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+      itemViewTwo.heightAnchor.constraint(equalToConstant: itemHeight),
+    ])
+  }
+  
+  func add(childVC: UIViewController, to containerView: UIView) {
+    addChild(childVC)
+    containerView.addSubview(childVC.view)
+    childVC.view.frame = containerView.bounds
+    childVC.didMove(toParent: self)
   }
   
   @objc func dismissVC() {
---
+++ GFFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GFFollowers
+//
+//  Created by Sean Lee on 8/16/24.
+//
+
+import Foundation
+
+enum SFSymbols {
+  static let location = "mappin.and.ellipse"
+}
